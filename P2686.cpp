0cbccc598cda0b07992db2c47b104aad727905b1
yuyu1184 <79351958+yuyu1184@users.noreply.github.com>
2022-01-18 08:01:39 +0800

    Update P2686.cpp

--- P2686.cpp
+++ P2686.cpp
@@ -1,1 +1,49 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long LL;
+
+inline int lowbit(int x)
+{
+    return x & (-x);
+}
+
+int n, m;
+int tree[200010];
+
+inline void add(int x, int k)
+{
+    while (x <= n)
+    {
+        tree[x] += k;
+        x += lowbit(x);
+    }
+}
+
+inline LL sum(int x)
+{
+    LL ans = 0;
+    while (x)
+    {
+        ans += tree [x];
+        x -= lowbit(x);
+    }
+    return ans;
+}
+
+signed main()
+{
+    cin >> n >> m;
+    while (m --)
+    {
+        char s[2];
+        scanf ("%s", sd);
+        int y = read(), z = read();
+        if (*s == 'x')
+            add(y, z);
+        else
+            cout << sum(z) - sum(y - 1) << endl;
+    }
+    return 0;
+}
